6c026b4e30e4c814254ea498a1aef3045d4ce94b
Fluffy515 <owenoberg06@hotmail.com>
2026-01-20 15:15:04 -0600

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "workout-tracker-v1";
+
+// Add anything you want cached for offline use here:
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
